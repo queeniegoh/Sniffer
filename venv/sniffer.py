dbbc986f487be3a4ea043157d309413de3fa83a8
queeniegoh <goh_minqi_queenie@connect.ite.edu.sg>
2021-07-22 12:31:24 +0000

    update 2

--- venv/sniffer.py
+++ venv/sniffer.py
@@ -1,2 +1,14 @@
 import socket
 
+s = socket.socket()
+print("Socket created")
+port = 4567
+s.bind((",port"));
+
+print("Socket bind to %s" %(port))
+
+s.listen(5);
+print("Socket is listening...");
+while True:
+    c, addr=s.accept();
+    print("Connection from",addr)
