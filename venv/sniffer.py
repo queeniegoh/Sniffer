d9f0e312995fa97519524b32590056757527227f
queeniegoh <goh_minqi_queenie@connect.ite.edu.sg>
2021-07-22 12:37:53 +0000

    update 3

--- venv/sniffer.py
+++ venv/sniffer.py
@@ -3,10 +3,8 @@ import socket
 s = socket.socket()
 print("Socket created")
 port = 4567
-s.bind((",port"));
-
-print("Socket bind to %s" %(port))
-
+s.bind((",port));
+print("Socket bind to %s" %(port));
 s.listen(5);
 print("Socket is listening...");
 while True:
